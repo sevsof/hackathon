dbfbe80c78ef26439f623ebefb8abfe9b0552d85
Солодовникова Анастасия <asolodovnikova@edu.hse.ru>
2026-06-01 19:25:23 +0300

    добавлено создание html отчета

--- src/main.py
+++ src/main.py
@@ -2,6 +2,10 @@ from reader import EmailReader
 from logger import MailLogger
 from moveToFolder import MoveToFolder
 from classifier import Classifier
+from visualization import form_stats, bar_chart, pie_chart, generate_html_report
+import webbrowser
+import os
+
 def main():
     print("="*60)
     print("АВТОМАТИЗИРОВАННАЯ СИСТЕМА ОБРАБОТКИ КОРПОРАТИВНОЙ ПОЧТЫ")
@@ -24,6 +28,20 @@ def main():
         print(f"{x.filename} в категории {categ}" )
     mail_logg.save_logs()
     mail_logg.save_stats()
+    
+    try:
+        categories, total_emails = form_stats("stats.json")
+        
+        bar_chart(categories)
+        pie_chart(categories)
+        generate_html_report(categories, total_emails, template_name="template.html", report_name="report.html")
+        file_path = f"file://{os.path.abspath('report.html')}"
+        
+        print("Открываю отчет в браузере...")
+        webbrowser.open(file_path)
+        
+    except Exception as e:
+        print(f"[Ошибка] Не удалось сгенерировать отчет: {e}")
     print("="*60)
     print('ПРОГРАММА УСПЕШНО ЗАВЕРШЕНА')
     print("="*60)
--- visualization.py
+++ visualization.py
@@ -1,11 +1,14 @@
 import json
 import matplotlib.pyplot as plt
+from string import Template
+
 
 def form_stats(name="stats.json"):
     with open(name,encoding="utf-8") as file:
         info = json.load(file)
     categories = {k: v for k,v in info.items() if k!="total"}
-    return categories
+    total_emails = info.get("total", sum(categories.values()))
+    return categories, total_emails
 
 def bar_chart(categories):
     plt.figure(figsize=(8,5))
@@ -22,11 +25,34 @@ def pie_chart(categories):
     plt.title("Круговая диаграмма для иллюстрации распределения писем по категориям")
     plt.savefig("pie_chart.png")
     plt.close()
+    
+    
+def generate_html_report(categories, total_emails, template_name="template.html", report_name="report.html"):
+    table_rows = ""
+    for category, count in sorted(categories.items(), key=lambda x: x[1], reverse=True):
+        table_rows += f"<tr><td>{category}</td><td>{count}</td></tr>\n"
+
+    with open(template_name, "r", encoding="utf-8") as file:
+        template_content = file.read()
+
+
+    template = Template(template_content)
+    final_html = template.substitute(
+        total_emails=total_emails,
+        table_rows=table_rows
+    )
+
+
+    with open(report_name, "w", encoding="utf-8") as file:
+        file.write(final_html)
+    
+    print(f"Отчет успешно сгенерирован: {report_name}")
 
 def main():
-    categories = form_stats()
+    categories, total_emails = form_stats()
     bar_chart(categories)
     pie_chart(categories)
+    generate_html_report(categories, total_emails)
     
 
 if __name__ == "__main__":
