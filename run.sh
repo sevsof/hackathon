4bef3966d091e0c9973356bddf0ad3133932d47e
rimfildsonya-commits <rimfildsonya@gmail.com>
2026-06-01 08:50:00 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash #https://habr.com/ru/articles/47163/
-if [-d 'inbox'] 
-then python3 src/main.py
+if [-d 'inbox']; then 
+    python3 src/main.py
 else
-    echo "Данного файла нет, ошибка'
+    echo "Данного файла нет, ошибка"
+fi
